e8e3ee410555406ae098a1754c04ac49d2156034
Artem Voytenko <bobinrobin12@gmail.com>
2023-01-22 19:12:07 +0300

    Написал вариант решения второй задачи

--- Task2/max from three.cs
+++ Task2/max from three.cs
@@ -1,2 +1,22 @@
-﻿Console.Clear(); //Очищаем консоль перед выводом информации
+﻿Console.Clear(); // Очистили консоль перед выводом ответа
+Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine()); 
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine()); 
+Console.Write("Введите третье числ: ");
+int numberC = int.Parse(Console.ReadLine()); 
 
+if (numberA > numberB & numberA > numberC)
+{
+    int max = numberA;
+}
+if (numberB > numberA & numberB > numberC)
+{
+    int max = numberB;
+}
+else
+{
+    int max = numberC;
+}
+Console.Write("max = ");
+Console.WriteLine(max);
