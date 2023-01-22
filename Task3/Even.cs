c4e0e045f8536b76075f625940a706e0cd98b9c0
Artem Voytenko <bobinrobin12@gmail.com>
2023-01-22 19:33:51 +0300

    Исправил, теперь работает

--- Task3/Even.cs
+++ Task3/Even.cs
@@ -1,6 +1,6 @@
 ﻿Console.Clear(); // И снова чистим консоль перед выводом информации
-Console.write("Введите число: ");
-number = Console.ReadLine();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
 if (number % 2 == 0)
 {
     Console.WriteLine("Число чётное");
